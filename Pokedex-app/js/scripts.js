628b66ed394753b9d91778a3f07d3aa5c252e0b3
LJ Hamilton <57067351+ljham1989@users.noreply.github.com>
2019-12-22 18:47:43 -0500

    Add files via upload
    init upload

---
+++ Pokedex-app/js/scripts.js
@@ -0,0 +1,34 @@
+var repositoryArray = [
+  { name: "Bulbasaur", height: 0.7, type: "grass" },
+  { name: "Ivysaur", height: 1, type: "grass" },
+  { name: "Venusaur", height: 1, type: "grass" }
+];
+
+document.write('Balbasaur (height: .7')
+document.write('Ivysaur (height: 1')
+document.write('Venusaur (height: 2')
+
+var repository = 0.7
+var result;
+if (height > 0.7) {
+    result = 'Wow, that's big!';
+}
+
+var repository = [
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Ivysaur',
+        height: 1,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Venusaur',
+        height: 2,
+        types: ['grass', 'poison']
+    }
+];
+
