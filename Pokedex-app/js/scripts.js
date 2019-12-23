04c9a0c8a777d1eb9843942c8d4c3b6641820ce2
LJ Hamilton <57067351+ljham1989@users.noreply.github.com>
2019-12-23 07:09:02 -0500

    Removed extra array

--- Pokedex-app/js/scripts.js
+++ Pokedex-app/js/scripts.js
@@ -14,21 +14,4 @@ if (height > 0.7) {
     result = 'Wow, that's big!';
 }
 
-var repository = [
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Ivysaur',
-        height: 1,
-        types: ['grass', 'poison']
-    },
-    {
-        name: 'Venusaur',
-        height: 2,
-        types: ['grass', 'poison']
-    }
-];
 
