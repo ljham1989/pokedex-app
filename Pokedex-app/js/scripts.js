a3c20b6b980e4543ef3ae018fb569f2c2519856a
LJ Hamilton <57067351+ljham1989@users.noreply.github.com>
2019-12-22 19:33:32 -0500

    Update scripts.js
    corrected var

--- Pokedex-app/js/scripts.js
+++ Pokedex-app/js/scripts.js
@@ -8,7 +8,7 @@ document.write('Balbasaur (height: .7')
 document.write('Ivysaur (height: 1')
 document.write('Venusaur (height: 2')
 
-var repository = 0.7
+var = 'repository';
 var result;
 if (height > 0.7) {
     result = 'Wow, that's big!';
