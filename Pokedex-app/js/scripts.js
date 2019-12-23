befcb77b4093ebe39d0fe1a02f7855a5170402b7
LJ Hamilton <57067351+ljham1989@users.noreply.github.com>
2019-12-23 07:47:48 -0500

    Update scripts.js

--- Pokedex-app/js/scripts.js
+++ Pokedex-app/js/scripts.js
@@ -4,14 +4,13 @@ var repositoryArray = [
   { name: "Venusaur", height: 1, type: "grass" }
 ];
 
-document.write('Balbasaur (height: .7')
-document.write('Ivysaur (height: 1')
-document.write('Venusaur (height: 2')
+document.write('Balbasaur (height: .7)')
+document.write('Ivysaur (height: 1)')
+document.write('Venusaur (height: 2)')
 
-var = 'repository';
-var result;
 if (height > 0.7) {
     result = 'Wow, that's big!';
 }
+
 
 
